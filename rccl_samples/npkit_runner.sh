885bcf7a0ce5d2c07f1012bf8aa686213d100f72
Ziyue Yang <ziyyang@microsoft.com>
2022-06-20 00:40:56 +0000

    add rccl samples

---
+++ rccl_samples/npkit_runner.sh
@@ -0,0 +1,53 @@
+set -x
+
+# Function that runs rccl-tests and collect NPKit traces.
+# rccl_test
+#   <rccl_test_bin> <msg_size> <algorithm> <protocol> <num_warmups> <num_iters>
+#   <npkit_dump_dir> <npkit_result_dir>
+function rccl_test() {
+  mpirun \
+    -np 16 -host localhost:16 \
+    -env LD_LIBRARY_PATH=$2/build:$LD_LIBRARY_PATH \
+    -env NCCL_DEBUG=INFO \
+    -env NCCL_DEBUG_SUBSYS=INIT,GRAPH \
+    -env NCCL_ALGO=$4 \
+    -env NCCL_PROTO=$5 \
+    -env NPKIT_DUMP_DIR=$8 \
+    $1 -b $3 -e $3 -f 2 -g 1 -c 1 -w $6 -n $7 | tee $9/log.txt
+}
+
+# Tag of this NPKit run.
+npkit_run_tag=`basename ${RCCL_TEST_BIN}`"/${msg_size}/${RCCL_ALGO}/${RCCL_PROTO}"
+
+# Path to NPKit dump directory.
+npkit_dump_dir="${NPKIT_RUN_DIR}/npkit_dump/${npkit_run_tag}"
+
+# Path to NPKit post-process directory.
+npkit_trace_dir="${NPKIT_RUN_DIR}/npkit_trace/${npkit_run_tag}"
+
+# Path to NPKit result directory.
+npkit_result_dir="${NPKIT_RUN_DIR}/npkit_result/${npkit_run_tag}"
+
+# Build RCCL with NPKit
+cd ${RCCL_SRC_DIR}
+rm -rf build
+mkdir -p build
+cd build
+CXX=/opt/rocm/bin/hipcc cmake -DNPKIT_FLAGS="${NPKIT_FLAGS}" ..
+make -j
+
+# Clean existing results
+rm -rf ${NPKIT_RUN_DIR}
+mkdir -p ${npkit_dump_dir}
+mkdir -p ${npkit_trace_dir}
+mkdir -p ${npkit_result_dir}
+
+# Run NPKit on all nodes.
+rccl_test ${RCCL_TEST_BIN} ${RCCL_SRC_DIR} ${RCCL_MSG_SIZE} ${RCCL_ALGO} ${RCCL_PROTO} ${RCCL_NUM_WARMUPS} ${RCCL_NUM_ITERS} ${npkit_dump_dir} ${npkit_result_dir}
+
+# Generate trace file
+cd ${NPKIT_SRC_DIR}/rccl_samples
+python3 npkit_trace_generator.py --npkit_dump_dir=${npkit_dump_dir} --npkit_event_header_path=${RCCL_SRC_DIR}/src/include/npkit/npkit_event.h --output_dir=${npkit_trace_dir}
+cd ${npkit_trace_dir}
+tar cvzf npkit_result.tar.gz npkit_event_trace.json
+mv npkit_result.tar.gz ${npkit_result_dir}
