6c058e8e78f9368cf9b599014fdee76abba1c621
Ziyue Yang <yzylivezh@hotmail.com>
2023-03-21 02:41:42 +0000

    Revise Code - Update NCCL Example

---
+++ nccl_samples/README.md
@@ -0,0 +1,19 @@
+## Introduction
+
+This folder contains scripts for NPKit sample workflow for NCCL. The sample workflow first builds NCCL with NPKit enabled, then runs nccl-tests to collect NPKit event dump files, and finally generates NPKit trace file.
+
+## Dependencies
+
+[NCCL 2.17.1-1](https://github.com/nvidia/nccl/tree/v2.17.1-1) and [nccl-tests](https://github.com/nvidia/nccl-tests).
+
+## Usage
+
+1) Get NCCL version 2.17.1-1 and apply `npkit-for-nccl-2.17.1-1.diff` to the source repo.
+
+2) Make sure parameters in `npkit_launcher.sh` are valid. Also note that currently NPKit only supports collecting non-overlapped events in GPU, and `NPKIT_FLAGS` should follow this rule.
+
+3) Make sure `nccl_test` function in `npkit_runner.sh` is a valid command to run `nccl-tests` binary. Also note that currently NPKit only supports 1 GPU per process, so `-g 1` mode is required in `nccl-tests` commands.
+
+4) Run command `bash npkit_launcher.sh`.
+
+5) The generated trace file `npkit_event_trace.json` (zipped in `npkit_result.tar.gz`) is in [Google Trace Event Format](https://docs.google.com/document/d/1CvAClvFfyA5R-PhYUmn5OOQtYMH4h6I0nSsKchNAySU/preview) and can be viewed by trace viewers.
--- nccl_samples/npkit_runner.sh
+++ nccl_samples/npkit_runner.sh
@@ -4,23 +4,22 @@
 set -x
 
 # Function that runs nccl-tests and collect NPKit traces.
-# msccl_test
+# nccl_test
 #   <nccl_test_bin> <msg_size> <algorithm> <protocol> <num_warmups> <num_iters>
 #   <npkit_dump_dir> <npkit_result_dir>
-function msccl_test() {
+function nccl_test() {
   mpirun --allow-run-as-root \
     -map-by ppr:8:node --bind-to numa \
-    -x LD_PRELOAD=$2/build/lib/libnccl.so.2:$LD_PRELOAD \
+    -x LD_PRELOAD=$2/build/lib/libnccl.so:$LD_PRELOAD \
     -x NCCL_DEBUG=WARN \
     -x NCCL_ALGO=$4 \
     -x NCCL_PROTO=$5 \
     -x NPKIT_DUMP_DIR=$8 \
-    -x MSCCL_XML_FILES=${10} \
     $1 -b $3 -e $3 -f 2 -g 1 -c 1 -w $6 -n $7 | tee $9/log.txt
 }
 
 # Tag of this NPKit run.
-npkit_run_tag=`basename ${NCCL_TEST_BIN}`"/${msg_size}/${MSCCL_ALGO}/${MSCCL_PROTO}"
+npkit_run_tag=`basename ${NCCL_TEST_BIN}`"/${msg_size}/${NCCL_ALGO}/${NCCL_PROTO}"
 
 # Path to NPKit dump directory.
 npkit_dump_dir="${NPKIT_RUN_DIR}/npkit_dump/${npkit_run_tag}"
@@ -31,13 +30,10 @@ npkit_trace_dir="${NPKIT_RUN_DIR}/npkit_trace/${npkit_run_tag}"
 # Path to NPKit result directory.
 npkit_result_dir="${NPKIT_RUN_DIR}/npkit_result/${npkit_run_tag}"
 
-# Path to MSCCL algorithm XML.
-msccl_algo_xml="${NPKIT_SRC_DIR}/msccl_samples/msccl_algo_sample.xml"
-
-# Build MSCCL with NPKit
-cd ${MSCCL_SRC_DIR}
+# Build NCCL with NPKit
+cd ${NCCL_SRC_DIR}
 make clean
-make -j src.build NPKIT=1
+make -j src.build NPKIT_FLAGS="${NPKIT_FLAGS}"
 
 # Clean existing results
 rm -rf ${NPKIT_RUN_DIR}
@@ -46,11 +42,11 @@ mkdir -p ${npkit_trace_dir}
 mkdir -p ${npkit_result_dir}
 
 # Run NPKit on all nodes.
-msccl_test ${NCCL_TEST_BIN} ${MSCCL_SRC_DIR} ${MSCCL_MSG_SIZE} ${MSCCL_ALGO} ${MSCCL_PROTO} ${MSCCL_NUM_WARMUPS} ${MSCCL_NUM_ITERS} ${npkit_dump_dir} ${npkit_result_dir} ${msccl_algo_xml}
+nccl_test ${NCCL_TEST_BIN} ${NCCL_SRC_DIR} ${NCCL_MSG_SIZE} ${NCCL_ALGO} ${NCCL_PROTO} ${NCCL_NUM_WARMUPS} ${NCCL_NUM_ITERS} ${npkit_dump_dir} ${npkit_result_dir}
 
 # Generate trace file
-cd ${NPKIT_SRC_DIR}/msccl_samples
-python3 npkit_trace_generator.py --npkit_dump_dir=${npkit_dump_dir} --npkit_event_header_path=${MSCCL_SRC_DIR}/src/include/npkit/npkit_event.h --output_dir=${npkit_trace_dir}
+cd ${NPKIT_SRC_DIR}/nccl_samples
+python3 npkit_trace_generator.py --npkit_dump_dir=${npkit_dump_dir} --npkit_event_header_path=${NCCL_SRC_DIR}/src/include/npkit/npkit_event.h --output_dir=${npkit_trace_dir}
 cd ${npkit_trace_dir}
 tar cvzf npkit_result.tar.gz npkit_event_trace.json
 mv npkit_result.tar.gz ${npkit_result_dir}
